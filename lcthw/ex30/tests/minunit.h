28cd7f7b8caa4bad80281af91c4da98c27274d00
--global <1157374677@qq.com>
2023-03-21 15:49:18 +0800

    modified ex30

--- lcthw/ex30/tests/minunit.h
+++ lcthw/ex30/tests/minunit.h
@@ -8,7 +8,7 @@
 
 #define mu_suite_start() char *message = NULL
 
-#define mu_assert(test, message) if(!(test)) { log_error("mu_assert", message); return message; }
+#define mu_assert(test, message) if(!(test)) { log_err("mu_assert", message); return message; }
 #define mu_run_test(test) debug("\n-----%s", " "#test); \
 	message = test(); tests_run++; if(message) return message;
 
