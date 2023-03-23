0df665cc96bae488eaef90ad43c5caf9337fb65d
--global <1157374677@qq.com>
2023-03-24 00:20:53 +0800

    add queue.h in liblcthw

---
+++ lcthw/liblcthw/src/lcthw/queue.h
@@ -0,0 +1,16 @@
+#ifndef lcthw_Queue_h
+#define lcthw_Queue.h
+
+#include <lcthw/list.h>
+
+#define Queue List
+#define Queue_create List_create
+#define Queue_destroy List_destroy
+#define Queue_send List_push
+#define Queue_peek List_first
+#define Queue_count List_count
+#define Queue_recv List_shift
+
+#define QUEUE_FOREACH(Q, V) LIST_FOREACH(Q, first, next, V)
+
+#endif
--- lcthw/liblcthw/tests/minunit.h
+++ lcthw/liblcthw/tests/minunit.h
@@ -9,6 +9,9 @@
 #define mu_suite_start() char *message = NULL
 
 #define mu_assert(test, message) if(!(test)) { log_err("mu_assert", message); return message; }
+// 这里是说在我们刚开头要mu_suite_start创建一个message为了mu_run_test调用
+// 在调用的时候，如果test错误，由mu_assert返回错误信息到mu_run_test
+// 否则函数成功就会返回NULL，就不返回信息了
 #define mu_run_test(test) debug("\n-----%s", " "#test); \
 	message = test(); tests_run++; if(message) return message;
 
